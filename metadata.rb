5966701b6d99eabb62016800506f5f413bfe8ad6
Nigel Wright <hertzz@gmail.com>
2019-08-02 13:40:47 +1200

    Updated license in metadata

--- metadata.rb
+++ metadata.rb
@@ -1,20 +1,10 @@
-name 'actifio'
+name 'actifio-chef'
 maintainer 'Nigel Wright'
 maintainer_email 'nigel.wright@dimensiondata.com'
-license 'All Rights Reserved'
+license 'Apache-2.0'
 description 'Allows for host registration and mounting of data from Actifio'
 long_description 'Allows for host registration and mounting of data from Actifio'
 version '3.0.34'
 chef_version '>= 12.1' if respond_to?(:chef_version)
-
-# The `issues_url` points to the location where issues for this cookbook are
-# tracked.  A `View Issues` link will be displayed on this cookbook's page when
-# uploaded to a Supermarket.
-#
-# issues_url 'https://github.com/<insert_org_here>/actifio/issues'
-
-# The `source_url` points to the development repository for this cookbook.  A
-# `View Source` link will be displayed on this cookbook's page when uploaded to
-# a Supermarket.
-#
-# source_url 'https://github.com/<insert_org_here>/actifio'
+issues_url 'https://github.com/nwright-nz/chef-actifio/issues'
+source_url 'https://github.com/nwright-nz/chef-actifio'
